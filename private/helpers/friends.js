fe95e625194f0a110da34b345476fe5f412d09ef
Grant Timmerman <granttimmerman@gmail.com>
2014-07-19 16:54:54 -0700

    Add friend function

---
+++ private/helpers/friends.js
@@ -0,0 +1,34 @@
+
+// Gets friends who are online
+//
+// Example:
+//
+// require('./private/helpers/friends').getOnlineFriends([], function (friends) {
+//   console.log('friends:');
+//   console.log(friends);
+// });
+module.exports.getOnlineFriends = function (friends, cb) {
+  // Get all online users
+  var User = require('../models/user');
+  User.find({ online: true }, function (users) {
+    var onlineFriends = [];
+    for (var i in users) {
+      var user = users[i];
+      var userId = user.id;
+
+      var isFriend = false;
+      for (var j in friends) {
+        var friend = friend[j];
+        var friendId = friend.id;
+        if (userId === friendId) {
+          isFriend = true;
+        }
+      }
+
+      if (isFriend) {
+        onlineFriends.push(user);
+      }
+    }
+    cb(onlineFriends);
+  });
+};
