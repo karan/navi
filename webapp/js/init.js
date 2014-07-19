d7110f3f636e86b8481b3b69ad1c1667a5aec5d8
Aaron Nech <aaronnech@gmail.com>
2014-07-19 00:39:25 -0700

    fixed

--- webapp/js/init.js
+++ webapp/js/init.js
@@ -1,10 +1,14 @@
 var app = new AppViewModel(facebookUser);
+
+
 ko.applyBindings(app, document.getElementById('app'));
 if (app.isAuthenticated()) {
 	app.setScreen(SCREEN_TYPE.CHOOSE);
 } else {
 	app.setScreen(SCREEN_TYPE.LOGIN);
 }
+
+
 var socket = io();
 socket.on('connect', function(){
   socket.send('data', 'test');
