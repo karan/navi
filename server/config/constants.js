119cafba81b272b06e6816b7b30491a27f89ef01
Karan Goel <karan@goel.im>
2014-07-19 13:49:03 -0700

    add config file

---
+++ server/config/constants.js
@@ -0,0 +1,6 @@
+var Constants = {
+  MONGOLAB_URI: 'mongodb://heroku_app27593770:si9agmbohbtdia6lh6nq2lqk9o@ds043467.mongolab.com:43467/heroku_app27593770',
+  REDISTOGO_URL: 'redis://redistogo:c682cda4001d6f504e5829627a4406a5@soldierfish.redistogo.com:10038/',
+}
+
+module.exports = Constants;
