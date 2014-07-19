5c7bfe249078d71b0f84c7d909b9b052ce342f60
Grant Timmerman <granttimmerman@gmail.com>
2014-07-19 16:12:56 -0700

    Add client-side socket code

--- webapp/js/init.js
+++ webapp/js/init.js
@@ -1,2 +1,10 @@
 var app = new AppViewModel();
 ko.applyBindings(app, document.getElementById('app'));
+
+var socket = io();
+io.on('connection', function(socket){
+  console.log('a user connected');
+  socket.on('disconnect', function(){
+    console.log('user disconnected');
+  });
+});
