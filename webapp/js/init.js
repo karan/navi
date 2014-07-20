6c2f2e99c70390bc5fd67af70f6cb43659ca7ba8
Karan Goel <karan@goel.im>
2014-07-20 05:19:48 -0700

    made code sharing work

--- webapp/js/init.js
+++ webapp/js/init.js
@@ -14,16 +14,18 @@ socket.on('connect', function(){
 
   // connect user to room called his id
   console.log("connecting user from client ");
+
   socket.emit('joinRoom', app.getUser()._id);
 
-  socket.send('data', 'test');
-  socket.on('event', function(data){});
-  socket.on('disconnect', function(){});
+  // socket.send('data', 'test');
+  // socket.on('event', function(data){});
+  // socket.on('disconnect', function(){});
 
   socket.on('connectToGame', function(game) {
     console.log("I'm the other client " + socket);
     console.log("connecting to game session ");
     console.log(game);
+    app.setScreen(SCREEN_TYPE.CODE, {'type' : MODE.RANDOM, 'game' : game});
   });
 
 });
